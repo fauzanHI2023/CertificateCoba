5319e3b2efc9223306c43c78ff75244c8ff8ffdd
fauzanHI2023 <fauzan.aldi@human-initiative.org>
2023-11-09 14:19:44 +0700

    update data

--- index.js
+++ index.js
@@ -48,35 +48,3 @@ const generatePDF = async (name) => {
   const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
   saveAs(pdfDataUri,"newcertificate.pdf")
 };
-
-        const dataForm = document.getElementById("dataForm");
-        const nameInput = document.getElementById("nameInput");   
-        const dataTable = document.getElementById("dataTable");
-        let data = JSON.parse(localStorage.getItem("savedData")) || [];
-
-        data.forEach(item => appendToTable(item.name));
-
-        dataForm.addEventListener("submit", function (e) {
-            e.preventDefault();
-            const name = nameInput.value.trim();
-
-            if (name !== "") {
-                data.push({ name });
-                appendToTable(name);
-                nameInput.value = "";
-                saveDataToLocalStorage();
-            }
-        });
-
-        function appendToTable(name) {
-            const tableBody = dataTable.querySelector("tbody");
-            const row = document.createElement("tr");
-            row.innerHTML = `<td>${name}</td>`;
-            tableBody.appendChild(row);
-        }
-
-        function saveDataToLocalStorage() {
-            localStorage.setItem("savedData", JSON.stringify(data));
-        }
-
-
